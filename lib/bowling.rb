739d15683191c492cfed1328a6ffc5937f4fed6a
Benjamin Winkler <ben@hardboiledgeek.com>
2012-01-15 00:33:20 -0500

    Added first three tests

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,5 +1,25 @@
 class Bowling
-  def add x, y
-    x + y
+  
+  def initialize
+    @score = 0
+    @rolls = []
   end
+  
+  def roll (pins)
+    @rolls << pins
+  end
+  
+  def score
+    while @rolls.length > 0
+      roll_one = @rolls.slice!(0)
+      roll_two = @rolls.slice!(0)
+      if (roll_one + roll_two == 10) then
+        @score += (10 + @rolls[0])
+      else
+        @score += (roll_one + roll_two)
+      end
+    end
+    @score
+  end
+  
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,8 +2,21 @@ require 'bowling'
 
 describe Bowling do
 
-  it "adds" do
-    subject.add(2, 2).should == 4
+  it "scores a zero when all zeroes are rolled" do
+	  subject.score.should == 0
+  end
+  
+  it "scores 20 when all ones are rolled" do
+    20.times do subject.roll(1) end
+    subject.score.should == 20
+  end
+  
+  it "will score a spare" do
+    subject.roll(4)
+    subject.roll(6)
+    subject.roll(2)
+    17.times do subject.roll(1) end
+    subject.score.should == 31
   end
 
 end
