f2e68827fd0c45653f70f5c535cba7d0ffc4497f
Benjamin Winkler <ben@hardboiledgeek.com>
2012-01-16 16:10:38 -0500

    Seven tests;  kata finished.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,8 +2,8 @@ class Bowling
   
   def initialize
     @score = 0
+    @frames = 0
     @rolls = []
-    @frames = []
   end
   
   def roll (pins)
@@ -11,20 +11,29 @@ class Bowling
   end
   
   def score
-          
     while @rolls.length > 0
-      roll_one = @rolls.slice!(0)
-      if roll_one == 10 then
-        @score += (roll_one + @rolls[0] + @rolls[1])
+      @frames += 1
+      roll_one = @rolls.slice!(0) # every frame has at least one roll
+      if roll_one == 10 then # strike!
+        if (@frames == 10) then # strike in last frame, score and delete bonus rolls
+          @score += (roll_one + @rolls.slice!(0) + @rolls.slice!(0)) 
+        else
+          @score += (roll_one + @rolls[0] + @rolls[1])
+          @frames += 1
+          next # no more rolls in frame.  Advance to next execution.
+        end
         next
       end
-      roll_two = @rolls.slice!(0)
+      roll_two = @rolls.slice!(0) # if not strike, then frame had second roll
       @score += (roll_one + roll_two)
-      if (roll_one + roll_two == 10) then
-        @score += @rolls[0]
+      if (roll_one + roll_two == 10) then # spare!
+        if (@frames == 10) then # spare in last frame, score and delete bonus roll
+          @score += @rolls.slice!(0)
+        else
+          @score += @rolls[0]
+        end
       end
     end
     @score
   end
-  
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -26,11 +26,28 @@ describe Bowling do
     subject.score.should == 16
   end
   
+  it "will score a mixed game" do
+    frame(4,6)
+    frame(10)
+    frame(1,1)
+    frame(2,8)
+    frame(2,8)
+    frame(1,0)
+    4.times do frame(0,0) end
+    subject.score.should == 58
+  end
+    
   it "will score an end-of-game strike" do
-    9.times do frame(0,0) end
-    frame(10, 3, 4)
+    9.times do frame(0,0) end # nine gutterball frames
+    frame(10, 3, 4) # strike on last frame
     subject.score.should == 17
   end
+  
+  it "will score an end-of-game spare" do
+    9.times do frame(0,0) end # nine gutterball frames
+    frame(4,6,2) # spare on last frame
+    subject.score.should == 12
+    end
   
   def frame (*frame_rolls)
     while frame_rolls.length > 0
