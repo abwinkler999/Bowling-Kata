36ba8e1dbe8b040104320621955d6a8226b07efa
Benjamin Winkler <ben@hardboiledgeek.com>
2012-01-15 19:11:26 -0500

    Added test for end-of-game strike

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -3,6 +3,7 @@ class Bowling
   def initialize
     @score = 0
     @rolls = []
+    @frames = []
   end
   
   def roll (pins)
@@ -10,13 +11,17 @@ class Bowling
   end
   
   def score
+          
     while @rolls.length > 0
       roll_one = @rolls.slice!(0)
+      if roll_one == 10 then
+        @score += (roll_one + @rolls[0] + @rolls[1])
+        next
+      end
       roll_two = @rolls.slice!(0)
+      @score += (roll_one + roll_two)
       if (roll_one + roll_two == 10) then
-        @score += (10 + @rolls[0])
-      else
-        @score += (roll_one + roll_two)
+        @score += @rolls[0]
       end
     end
     @score
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,21 +3,40 @@ require 'bowling'
 describe Bowling do
 
   it "scores a zero when all zeroes are rolled" do
+    10.times do frame(0, 0) end
 	  subject.score.should == 0
   end
   
   it "scores 20 when all ones are rolled" do
-    20.times do subject.roll(1) end
+    10.times do frame(1,1) end
     subject.score.should == 20
   end
   
   it "will score a spare" do
-    subject.roll(4)
-    subject.roll(6)
-    subject.roll(2)
-    17.times do subject.roll(1) end
-    subject.score.should == 31
+    frame(4,6)
+    frame(2,0)
+    8.times do frame(0,0) end
+    subject.score.should == 14
   end
-
+  
+  it "will score a strike" do
+    frame(10)
+    frame(1,2)
+    8.times do frame(0,0) end
+    subject.score.should == 16
+  end
+  
+  it "will score an end-of-game strike" do
+    9.times do frame(0,0) end
+    frame(10, 3, 4)
+    subject.score.should == 17
+  end
+  
+  def frame (*frame_rolls)
+    while frame_rolls.length > 0
+      subject.roll(frame_rolls.slice!(0))
+    end
+  end
+  
 end
 
